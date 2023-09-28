0aee6c2dd1013753c382fce4131a3e1f28804447
Azam Amonov <azamamonov555@gmail.com>
2023-09-28 09:09:18 +0500

    finish toCase.py

--- Amaliyot_26_09/toCase.py
+++ Amaliyot_26_09/toCase.py
@@ -1,23 +1,19 @@
-camelCase = "camelCase"
-snakeCase = "snake_case"
-
-
 def toCase(case: str):
     if case.__contains__("_"):
         return toCamelCase(case)
     else:
         return toSnakeCase(case)
 
-    return "Your case is not a valid!"
-
 
 def toCamelCase(case: str):
-    for i in range(len(case)):
-        if case[i] == "_":
-            case[i + 1].upper()
-            case[i].replace("_", "")
-    resultCase = case
-    return resultCase
+
+    resultOfCase = []
+    var = case.split("_")
+    resultOfCase.append(var[0])
+
+    for i in range(1, len(var)):
+        resultOfCase.append(var[i].capitalize())
+    return "".join(resultOfCase)
 
 
 def toSnakeCase(case: str):
@@ -26,11 +22,19 @@ def toSnakeCase(case: str):
     for i in range(len(case)):
         if case[i].islower():
             resultOfCase.append(case[i])
+
         if case[i].isupper():
             resultOfCase.append("_")
             resultOfCase.append(case[i].lower())
 
     return "".join(resultOfCase)
 
-res = toSnakeCase(camelCase)
-print(res)
+camelCase = "camelCase"
+snakeCase = "snake_case"
+
+calelCase2 = "checkBoxQuestionType"
+snakeCase2 = "short_answer_question_type"
+
+print(toSnakeCase(calelCase2))
+print(toCamelCase(snakeCase2))
+print(toCase(calelCase2))
